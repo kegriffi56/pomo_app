07bc9450ed9731037def6674e8ef33df086a27ae
Kevin Griffith <kevingriffith@Griffith-MBP.attlocal.net>
2024-08-17 15:46:48 -0500

    initial junk commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
+
+@riverpod
+String kevin(KevinRef ref) => 'This is a test';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(kevinProvider);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$kevinHash() => r'968573d6cf74a8912396ed589504bdf44e308c87';
+
+/// See also [kevin].
+@ProviderFor(kevin)
+final kevinProvider = AutoDisposeProvider<String>.internal(
+  kevin,
+  name: r'kevinProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$kevinHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef KevinRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
